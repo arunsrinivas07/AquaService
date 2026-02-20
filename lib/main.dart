f8fed42b8dce9742bc733f664d08f925de867e93
VISHNU PRASATH PG <pgvishnu2006@gmail.com>
2026-02-20 23:09:08 +0530

    i am pushing the nav_bar and payment page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,55 @@
 import 'package:flutter/material.dart';
+import 'navbar/navbar.dart';
+import 'payment/payment.dart';
 
 void main() {
-  //runApp(const RoWaterApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 2; // Home selected by default
+
+  final List<Widget> _pages = [
+    const Center(child: Text("Profile Page")),
+    const PaymentSourceScreen(), // Index 1: Cash
+    const Center(child: Text("Home Page")), // Index 2: Home
+    const Center(child: Text("Status Page")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: CustomCurvedNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
 }
